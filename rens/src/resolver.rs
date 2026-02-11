bc19d5c699dc3840321281638b0c30ecd11e480f
nkemjika <nkemjikaohanyere@gmail.com>
2026-02-11 07:23:11 +0000

    feat: forward resolution

---
+++ rens/src/resolver.rs
@@ -0,0 +1,58 @@
+use super::name_hash::namehashing;
+use alloy::network::Ethereum;
+use alloy::primitives::{Address, address};
+use alloy::providers::{Provider, ProviderBuilder};
+use alloy::sol;
+use std::error::Error;
+
+#[derive(Debug)]
+pub struct EnsContractAddresses {
+    pub ens_registry: Address,
+    pub public_resolver: Address,
+    pub base_registrar: Address,
+    pub registrar_controller: Address,
+}
+
+impl EnsContractAddresses {
+    pub fn mainnet() -> Self {
+        Self {
+            ens_registry: address!("0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e"),
+            public_resolver: address!("0xF29100983E058B709F3D539b0c765937B804AC15"),
+            base_registrar: address!("0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85"),
+            registrar_controller: address!("0x253553366Da8546fC250F225fe3d25d0C782303b"),
+        }
+    }
+}
+
+sol! {
+    #[allow(missing_docs)]
+    #[sol(rpc)]
+    contract ENSRegistry {
+         function resolver(bytes32 node) public view virtual returns (address);
+    }
+}
+
+sol! {
+    #[allow(missing_docs)]
+    #[sol(rpc)]
+    contract PublicResolver {
+        function addr(bytes32 node) view returns (address);
+    }
+}
+
+pub async fn resolve_name(name: &str, provider: &impl Provider) -> Address {
+    let registry = ENSRegistry::new(EnsContractAddresses::mainnet().ens_registry, &provider);
+    let node = namehashing(name);
+    let resolver_address = registry
+        .resolver(node)
+        .call()
+        .await
+        .expect("Something went wrong");
+
+    let resolver_contract = PublicResolver::new(resolver_address, &provider);
+    resolver_contract
+        .addr(node)
+        .call()
+        .await
+        .expect("something went wrong")
+}
